5cf59c660c8fc54b685d703bccb235332684cf68
nicolezimmer <nicole.zimmer.epet20@gmail.com>
2022-10-04 15:36:43 -0300

    creo funcion flecha

--- js/script.js
+++ js/script.js
@@ -27,6 +27,7 @@ function sumaDosNumeros(n1, n2) {
 }
 //funciones fleha
 const multiplicarDosNumeros = (n1, n2) => resultado = num1 * num2;
+const dividirDosNumeros = (n1, n2) => resultado = num1 / num2;
 
 //invocar funciones
 
