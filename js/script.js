30178247891ca031c321ae376406db68fa73ca53
nicolezimmer <nicole.zimmer.epet20@gmail.com>
2022-10-04 15:37:46 -0300

    invoco funcion multiplicar

--- js/script.js
+++ js/script.js
@@ -30,6 +30,7 @@ const multiplicarDosNumeros = (n1, n2) => resultado = num1 * num2;
 const dividirDosNumeros = (n1, n2) => resultado = num1 / num2;
 //invocar funciones
 s = sumaDosNumeros(num1, num2);
+m = multiplicarDosNumeros(num1, num2);
 
 //mostrar resultado por consola
 //#endregion
